ce9327fd65f7413b77ef53c6d9886d7d73483619
Justin Savala <savala.justin@gmail.com>
2018-09-15 16:02:40 -0400

    Services are now listed in the collapsible rows.

--- src/containers/servers.js
+++ src/containers/servers.js
@@ -1,20 +1,71 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadData } from '../actions'
+import _ from 'lodash'
 
 class Servers extends Component {
     componentWillMount() {
         this.props.loadData()
     }
     render() {
-        let servers = this.props.servers
+        if (this.props.loading) {
+            let data = this.props.servers
+            let servers = Object.keys(data)
+            return(
+                <div id="serverList" className="serverlist">
+                    <ul className="collapsible">
+                        {servers.map((x, index) => {
+                            let server = data[x].HostName
+                            let services = data[x].Services.map((service, index) => {
+                                return(
+                                    <div key={index} className="collapsible-body">
+                                        <span>
+                                            {service.Description}
+                                        </span>
+                                    </div>  
+                                )
+                            })
+                            let svcQty = data[x].Services.length
+                            let svcRun = _.countBy(data[x].Services, 'Running').true || 0
+                            let allRunning = svcRun === svcQty
+                            let svcRunning = svcQty > 0 ? `${svcRun}/${svcQty}` : null
+                            let hosts = data[x].Hosts.map((host, index) => {
+                                return(
+                                    <span
+                                        key={index}
+                                        className={!host.Online ?
+                                            (index === 0 ? "new badge red": "new badge red altbadge") :
+                                            (index === 0 ? "new badge green" : "new badge green altbadge")}
+                                        data-badge-caption={host.Name}
+                                    />
+                                )
+                            })
+                            let svcBadge = <span
+                                                className={svcQty < 1 ?
+                                                    "hidden":
+                                                    (allRunning ? "new badge green altbadge" : "new badge red altbadge")}
+                                                data-badge-caption="Services">
+                                                    {svcRunning}
+                                            </span>
+                            return (
+                                <li key={index}>
+                                    <div className="collapsible-header">
+                                        <i className="material-icons">desktop_windows</i>
+                                        {server}
+                                        {hosts}
+                                        {svcBadge}
+                                    </div>
+                                    {services}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            )
+        }
         return(
             <div>
-                {Object.keys(servers).map(x => {
-                    return (
-                        <p>{x}</p>
-                    )
-                })}
+                Loading...
             </div>
         )
     }
@@ -30,7 +81,8 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = (state) => {
     return {
-        servers: state,
+        loading: state.Success,
+        servers: state.Servers,
     }
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,25 @@
 export const serverReducer = (
-    state = {}, 
+    state = {
+        "Success": false,
+        "Error": false,
+        "Servers": {"Loading": true}
+    }, 
     action) => {
     switch (action.type) {
         case "LOAD_DATA_SUCCESS":
-            console.log(action.data)
-            state = action.data
+            state = {
+                ...state,
+                "Success": true,
+                "Servers": action.data
+            }
             break;
         case "LOAD_DATA_FAILURE":
+            console.log("Error")
+            state = {
+                ...state,
+                "Servers": {"Error": true},
+                "Success": false,
+                "Error": true}
             break;
         default:
             break;
